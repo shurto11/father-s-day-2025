97b0a12172ac31e96efcd14fa6b1e9119dd5beeb
shurto11 <y.shuto0101@gmail.com>
2025-06-13 23:20:26 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,1 @@
-<button id="myButton">クリックしてみて</button>
-<script>
-    document.getElementById('myButton').addEventListener('click', function() {
-        alert('ボタンがクリックされました！');
-    });
-</script>
+
