45c0301171c5e474d6d5eaf846ff14226ed03b80
shurto11 <y.shuto0101@gmail.com>
2025-06-13 23:04:22 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+<button id="myButton">クリックしてみて</button>
+<script>
+    document.getElementById('myButton').addEventListener('click', function() {
+        alert('ボタンがクリックされました！');
+    });
+</script>
